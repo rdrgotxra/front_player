59c111516eb5db5c26736d87e3de39bd8c590cb1
Rodrigo Teixeira <rdrgotxra@gmail.com>
2024-06-12 23:33:17 -0300

    first

---
+++ scripts/songs.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        src: "./musicas/Depeche Mode - Enjoy The Silence.mp3",
+        name: "Depeche Mode - Enjoy The Silence",
+        image: "./imagens/depecheMode.jpg"
+    },
+    {
+        src: "./musicas/Hippie Sabotage - Your Soul.mp3",
+        name: "Hippie Sabotage - Your Soul",
+        image: "./imagens/hippieSabotageCapa.jpeg"
+    },
+    {
+        src: "./musicas/Pixies - Where Is My Mind (LegendadoTradução).mp3",
+        name: "Pixies - Where Is My Mind",
+        image: "./imagens/clubeDaLuta.jpg"
+    },
+];
